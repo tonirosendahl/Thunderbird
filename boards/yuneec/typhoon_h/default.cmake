d7f46fe82b9b5df2f77e7ad149b7161972dd7389
tonirosendahl <toni.rosendahl@gmail.com>
2019-11-12 23:45:51 +0200

    PX4 Typhoon H 480 initial commit

---
+++ boards/yuneec/typhoon_h/default.cmake
@@ -0,0 +1,76 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR yuneec
+	MODEL typhoon_h
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m4
+	ROMFSROOT px4fmu_common
+
+	SERIAL_PORTS
+
+	DRIVERS
+		adc
+		barometer/ms5611
+		#distance_sensor
+		gps
+		imu/mpu6000
+		magnetometer/hmc5883
+		px4fmu
+		rc_input
+		tap_esc
+		#telemetry # all available telemetry drivers
+		test_ppm
+		typhoon_bind
+
+	MODULES
+		attitude_estimator_q
+		commander
+		dataman
+		ekf2
+		events
+		land_detector
+		#landing_target_estimator
+		load_mon
+		#local_position_estimator
+		logger
+		mavlink
+		mc_att_control
+		mc_pos_control
+		navigator
+		sensors
+		sih
+		vmount
+		battery_status
+
+	SYSTEMCMDS
+		dmesg
+		config
+		hardfault_log
+		#led_control
+		mixer
+		motor_test
+		nshterm
+		param
+		perf
+		pwm
+		reflect
+		#tests # tests and test runner
+		top
+		topic_listener
+		tune_control
+		#usb_connected
+		ver
+
+	EXAMPLES
+		#bottle_drop # OBC challenge
+		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		#hwtest # Hardware test
+		#matlab_csv_serial
+		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		#rover_steering_control # Rover example app
+		#segway
+		#uuv_example_app
+
+	)
---
+++ src/drivers/typhoon_bind/typhoon_bind.h
@@ -0,0 +1,98 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2018 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <px4_module.h>
+#include <px4_module_params.h>
+#include <px4_config.h>
+#include <px4_getopt.h>
+#include <uORB/Subscription.hpp>
+#include <uORB/topics/parameter_update.h>
+#include <unistd.h>
+
+#include <drivers/drv_hrt.h>
+
+extern "C" __EXPORT int typhoon_bind_main(int argc, char *argv[]);
+
+
+int port_number;
+
+/**
+ *  Opens a device for use as UART.
+ *  @param device UNIX path of UART device
+ *  @param uart_fd file-descriptor of UART device
+ *  @return 0 on success, -1 on error
+ */
+int initialise_uart(const char *const device, int &uart_fd);
+
+/**
+ *  Closes a device previously opened with initialise_uart().
+ *  @param uart_fd file-descriptor of UART device as provided by initialise_uart()
+ *  @return 0 on success, -1 on error
+ */
+int deinitialise_uart(int &uart_fd);
+
+/**
+ *  Sends a packet to UART
+ *  @param uart_fd file-descriptor of UART device
+ */
+int send_packet(int uart_fd);
+
+
+
+class Typhoon_bind : public ModuleBase<Typhoon_bind>, public ModuleParams
+{
+public:
+	Typhoon_bind(int example_param, bool example_flag);
+
+	virtual ~Typhoon_bind() = default;
+
+	/** @see ModuleBase */
+	static int task_spawn(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static Typhoon_bind *instantiate(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int custom_command(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int print_usage(const char *reason = nullptr);
+
+	/** @see ModuleBase::run() */
+	void run() override;
+
+	/** @see ModuleBase::print_status() */
+	int print_status() override;
+};
